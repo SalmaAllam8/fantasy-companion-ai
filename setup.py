464339dc12899c4dda18b318856b05dc21548b61
Ahmed Abdelaziz <Ahmed.Abdelaziz.GM@gmail.com>
2024-02-23 19:56:02 +0200

    Initialized Project with CookieCutter, Data Science.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="ML model to predict the premier league stars' performance in the coming rounds.",
+    author='Salma Morsi & Ahmed Abdelaziz',
+    license='MIT',
+)
